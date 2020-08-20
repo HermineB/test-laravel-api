020617ecc2f5cf568d7f94cf45d7a3c832af4f90
Hermine <baghdassarianhermine@gmail.com>
2020-08-20 14:29:05 -0700

    auth directory

--- routes/api.php
+++ routes/api.php
@@ -13,19 +13,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/categories/add-categories', 'CategoriesController@store');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::post('/categories/add', 'CategoriesController@store');
-Route::post('/categories/update', 'CategoriesController@update');
-Route::post('/categories/delete', 'CategoriesController@deleteCategory');
+
+Route::middleware('auth:api')->post('/categories/add', 'CategoriesController@store');
+Route::middleware('auth:api')->post('/categories/update', 'CategoriesController@update');
+Route::middleware('auth:api')->post('/categories/delete', 'CategoriesController@deleteCategory');
+Route::middleware('auth:api')->post('/products/add', 'ProductsController@store');
+Route::middleware('auth:api')->post('/products/delete', 'ProductsController@delete');
+Route::middleware('auth:api')->post('/products/update', 'ProductsController@update');
 Route::get('/categories/get', 'CategoriesController@getCategories');
 Route::post('/products/get', 'ProductsController@getProducts');
 Route::get('/products/get', 'ProductsController@getProducts');
 Route::post('/products/getByCategory', 'ProductsController@getByCategory');
 Route::post('/products/getById', 'ProductsController@getById');
-Route::post('/products/add', 'ProductsController@store');
-Route::post('/products/delete', 'ProductsController@delete');
-Route::post('/products/update', 'ProductsController@update');
+
 
